3c96bb50dec3dff4181723eb69e2aa562452cd98
Nohjla <maligroaljhon@gmail.com>
2018-11-27 21:14:56 +0800

    update

--- demo10_addToCart/cart.php
+++ demo10_addToCart/cart.php
@@ -6,6 +6,9 @@
 	$dbname = "demoStoreNew";
 	// echo $_SESSION["item_count"];
 	foreach ($_SESSION["cart"] as $key => $value) {
-		
+		echo "<span>$key</span><input id='' type='number' value=$value><br>";
 	}	
 ?>
+<script>
+	
+</script>
